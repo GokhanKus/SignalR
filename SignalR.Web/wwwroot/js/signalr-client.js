a77273f46b896dadf763d291bffd4a7fed3b3ebb
Gokhan Kus <gkus1998@hotmail.com>
2024-11-21 10:21:45 +0300

    signalr-client.js (Connect to Hub)

---
+++ SignalR.Web/wwwroot/js/signalr-client.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    const connection = new signalR.HubConnectionBuilder().withUrl("/examplehub").configureLogging(signalR.LogLevel.Information).build();
+
+    function start() {
+        connection.start().then(() => console.log("hub ile baglanti kuruldu"));
+    }
+    try {
+        start();
+    }
+    catch {
+        setTimeout(()=>start(),5000)
+    }
+})
