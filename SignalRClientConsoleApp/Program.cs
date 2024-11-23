440036b5ec613e87cf20db882168663fc9fb7a28
Gokhan Kus <gkus1998@hotmail.com>
2024-11-23 14:09:00 +0300

    Console App (SignalR Client -2, console tarafindan gonderilen mesaji web tarafinda goruntelemek)

--- SignalRClientConsoleApp/Program.cs
+++ SignalRClientConsoleApp/Program.cs
@@ -14,4 +14,11 @@ connection.On<Product>("ReceiveComplexMessageForAllClient", product =>
 	Console.WriteLine($"Received Message: {product.Id} - {product.Name} - {product.Price}");
 });
 
-Console.ReadKey();
+while (true)
+{
+	var key = Console.ReadLine();
+	if (key == "exit") break;
+	var product = new Product(50, "pen 50", 200);
+	await connection.InvokeAsync("BroadcastComplexMessageToAllClient", product);
+	//await connection.InvokeAsync("BroadcastMessageToAllClient", key);
+}
