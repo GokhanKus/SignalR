f9b25a7c0d57513376ac2a5521d97147cd1364c1
Gokhan Kus <gkus1998@hotmail.com>
2024-11-23 13:32:15 +0300

    ConsoleApp (SignalR Client -1, web tarafindan gonderilen mesaji console ekranindan goruntuleme)

---
+++ SignalRClientConsoleApp/Product.cs
@@ -0,0 +1,1 @@
+﻿public record Product(int Id, string Name, decimal Price);
---
+++ SignalRClientConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("SignalR console client");
+
+var connection = new HubConnectionBuilder().WithUrl("https://localhost:7099/exampleTypeSafehub").Build();
+
+connection.StartAsync().ContinueWith(result =>
+{
+	Console.WriteLine(result.IsCompletedSuccessfully ? "Connected" : "Connection Failed");
+});
+
+connection.On<Product>("ReceiveComplexMessageForAllClient", product =>
+{
+	Console.WriteLine($"Received Message: {product.Id} - {product.Name} - {product.Price}");
+});
+
+Console.ReadKey();
